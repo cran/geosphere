c767fee3e074469e039149da6f4d19f3287d5a8b
Robert J. Hijmans <r.hijmans@gmail.com>
2010-07-25 00:00:00 +0000

    version 1.2-7

--- R/mercator.R
+++ R/mercator.R
@@ -7,14 +7,16 @@ mercator <- function(p, inverse=FALSE, r=6378137) {
 	toRad <- pi / 180 
 	if (inverse) {
 		p <- .pointsToMatrix(p, checkLonLat=FALSE) 
-		lat <- pi/2 - 2 * atan(exp(-p[,2] / r))
-		lon <- p[,1] / r 
-		return(cbind(lon, lat) / toRad)
+		p[ ,2] <- pi/2 - 2 * atan(exp(-p[,2] / r))
+		p[ ,1] <- p[,1] / r 
+		colnames(p) <- c('lon', 'lat')
+		return( p / toRad )
 	} else {
 		p <- .pointsToMatrix(p) * toRad
-		y <- log( tan(p[,2]) + (1 / cos(p[,2]))) * r
-		x <- p[,1] * r
-		return(cbind(x,y))
+		p[,2] <- log( tan(p[,2]) + (1 / cos(p[,2])))
+		p <- p * r
+		colnames(p) <- c('x', 'y')
+		return( p )
 	}
 }
 
